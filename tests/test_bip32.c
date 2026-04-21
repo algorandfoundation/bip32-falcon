f668aa2a4ae60084ff3341f0246ab6007ce358e0
ehanoc <ehanoc@protonmail.com>
2026-04-21 10:38:56 +0100

    chore: bip44 test keyGens, Signs and Verifies

--- tests/test_bip32.c
+++ tests/test_bip32.c
@@ -1,5 +1,6 @@
 #include "bip32_falcon.h"
 #include "../external/falcon/falcon.h"
+#include "../external/falcon/deterministic.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -131,87 +132,135 @@ int test_bip32_derive_and_get_falcon_keys(void)
 
 int bip44_test(void)
 {
-    bip32_node_t master_node;
-    const uint8_t root_seed[32] = {
-        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-        0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
-        0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
-        0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f
-    };
+  bip32_node_t master_node;
+  const uint8_t root_seed[32] = {
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+    0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+    0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+    0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f
+  };
 
-    int ret = bip32_master(&master_node, root_seed, sizeof(root_seed));
-    if (ret < 0) {
-        printf("FAIL: bip32_master returned %d\n", ret);
-        return -1;
-    }
+  int ret = bip32_master(&master_node, root_seed, sizeof(root_seed));
+  if (ret < 0) {
+    printf("FAIL: bip32_master returned %d\n", ret);
+    return -1;
+  }
 
-    const char *path = "m/44'/283'/0'/0/0";
-    bip32_node_t derived_node;
+  const char *path = "m/44'/283'/0'/0/0";
+  bip32_node_t derived_node;
 
-    printf("Deriving node at path %s...\n", path);
+  printf("Deriving node at path %s...\n", path);
 
-    ret = bip44_derive_path(&derived_node, &master_node, path);
-    if (ret < 0) {
-        printf("FAIL: bip32_derive_path returned %d\n", ret);
-        return -1;
-    }
+  ret = bip44_derive_path(&derived_node, &master_node, path);
+  if (ret < 0) {
+    printf("FAIL: bip32_derive_path returned %d\n", ret);
+    return -1;
+  }
 
-    printf("Derived node at path %s:\n", path);
-    printf("Depth: %u\n", derived_node.depth);
-    printf("Child number: %u\n", derived_node.child_number);
-    printf("Seed material: \n");
-    for (size_t i = 0; i < 32; i++) { 
-        printf("%02x", derived_node.seed_material[i]);
-    }
-    printf("\n");
+  printf("Derived node at path %s:\n", path);
+  printf("Depth: %u\n", derived_node.depth);
+  printf("Child number: %u\n", derived_node.child_number);
+  printf("Seed material: \n");
+  for (size_t i = 0; i < 32; i++) { 
+    printf("%02x", derived_node.seed_material[i]);
+  }
+  printf("\n");
 
-    // calculate falcon keys from the derived node
-    shake256_context key_rng;
-    shake256_init_prng_from_seed(&key_rng, derived_node.seed_material, 32);
-    
-    uint8_t privkey[FALCON_PRIVKEY_SIZE(10)];
-    uint8_t pubkey[FALCON_PUBKEY_SIZE(10)];
-    uint8_t tmp[FALCON_TMPSIZE_KEYGEN(10)];
+  // calculate falcon keys from the derived node
+  shake256_context key_rng;
+  shake256_init_prng_from_seed(&key_rng, derived_node.seed_material, 32);
 
-    ret = falcon_keygen_make(&key_rng, 10, privkey, sizeof(privkey),
-                             pubkey, sizeof(pubkey), tmp, sizeof(tmp));
-    if (ret != 0) {
-        printf("FAIL: falcon_keygen_make returned %d\n", ret);
-        return -1;
-    }
+  uint8_t pubkey[FALCON_DET1024_PUBKEY_SIZE];
+	uint8_t privkey[FALCON_DET1024_PRIVKEY_SIZE];
+	uint8_t signature[FALCON_DET1024_SIG_COMPRESSED_MAXSIZE];
+	size_t sig_len;
+	uint8_t expected_sig[FALCON_DET1024_SIG_COMPRESSED_MAXSIZE];
 
-    printf("Falcon-1024 keypair generated successfully from BIP44 path\n");
-   // printf(" Pvt Key: ");
-   // for (size_t i = 0; i < sizeof(privkey); i++) {
-   //     printf("%02x", privkey[i]);
-   // }
-   // printf("\n Pub Key: ");
-   // for (size_t i = 0; i < sizeof(pubkey); i++) {
-   //     printf("%02x", pubkey[i]);
-   // }
-   // printf("\n");
-    printf("  Private key size: %zu bytes\n", sizeof(privkey));
-    printf("  Public key size: %zu bytes\n", sizeof(pubkey));
-    printf("\n");
+  const char *msg = "Hello w0RlD";
+  size_t data_len = sizeof(msg) - 1;
+	uint8_t data[data_len];
 
-    printf("PASS: bip44_test\n");
-    return 0;
+	shake256_context msg_rng; // message RNG but deterministic? 
+	char msg_seed[8+1]; // msg seed?
+	sprintf(msg_seed, msg, data_len);
+	shake256_init_prng_from_seed(&msg_rng, msg_seed, 8);
+	shake256_extract(&msg_rng, data, data_len);
+
+  // fill with zeroes
+	memset(privkey, 0, FALCON_DET1024_PRIVKEY_SIZE);
+	memset(pubkey, 0, FALCON_DET1024_PUBKEY_SIZE);
+	memset(signature, 0, FALCON_DET1024_SIG_COMPRESSED_MAXSIZE);
+
+  ret = falcon_det1024_keygen(&key_rng, privkey, pubkey);
+  if (ret != 0) {
+    printf("FAIL: falcon_keygen_make returned %d\n", ret);
+    return -1;
+  }
+
+  printf("Falcon-1024 keypair generated successfully from BIP44 path\n");
+  // printf(" Pvt Key: ");
+  // for (size_t i = 0; i < sizeof(privkey); i++) {
+  //     printf("%02x", privkey[i]);
+  // }
+  // printf("\n Pub Key: ");
+  // for (size_t i = 0; i < sizeof(pubkey); i++) {
+  //     printf("%02x", pubkey[i]);
+  // }
+  // printf("\n");
+  printf("  Private key size: %zu bytes\n", sizeof(privkey));
+  printf("  Public key size: %zu bytes\n", sizeof(pubkey));
+  printf("\n");
+
+
+  // produce signature and verify it
+  ret = falcon_det1024_sign_compressed(signature, &sig_len, privkey, data, data_len);
+
+  if (ret != 0) {
+    printf("FAIL: falcon_sign_make returned %d\n", ret);
+    return -1;
+  }
+  printf("Signature generated successfully\n");
+
+  // print sig
+  printf(" Signature: ");
+  for (size_t i = 0; i < sig_len; i++) {
+      printf("%02x", signature[i]);
+  }
+  printf("\n");
+
+  // verify the signature
+	int v = falcon_det1024_get_salt_version(signature);
+	if (v != FALCON_DET1024_CURRENT_SALT_VERSION) {
+		fprintf(stderr, "unexpected salt version: %d", v);
+    return -1;
+	}
+
+	ret = falcon_det1024_verify_compressed(signature, sig_len, pubkey, data, data_len);
+	if (ret != 0) {
+		fprintf(stderr, "verify_compressed failed: %d\n", ret);
+	  return -1;
+	}
+
+  printf("Signature verified successfully\n");
+
+  printf("PASS: bip44_test\n");
+  return 0;
 }
 
 int main(void)
 {
-    int failures = 0;
+  int failures = 0;
 
-    failures += test_bip32_master_key();
-    failures += test_bip32_derive();
-    failures += test_bip32_derive_and_get_falcon_keys();
-    failures += bip44_test();
+  failures += test_bip32_master_key();
+  failures += test_bip32_derive();
+  failures += test_bip32_derive_and_get_falcon_keys();
+  failures += bip44_test();
 
-    if (failures == 0) {
-        printf("All tests PASSED!\n");
-        return 0;
-    } else {
-        printf("%d test(s) FAILED!\n", failures);
-        return 1;
-    }
+  if (failures == 0) {
+    printf("All tests PASSED!\n");
+    return 0;
+  } else {
+    printf("%d test(s) FAILED!\n", failures);
+    return 1;
+  }
 }
